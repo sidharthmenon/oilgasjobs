c3d0add8bd34f0c965384696b7cc4fffd2791620
sidharthmenon <sidharth.menon91@gmail.com>
2016-02-07 20:53:44 +0530

    re

--- www/js/index.js
+++ www/js/index.js
@@ -45,32 +45,39 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-        
-        var push = PushNotification.init({
-    android: {
-        senderID: "486403283342"
-    }
-});
-
-push.on('registration', function(data) {
-    // data.registrationId
-    alert("Reg id: "+data.registrationId);
-});
-
-push.on('notification', function(data) {
-    // data.message,
-    // data.title,
-    // data.count,
-    // data.sound,
-    // data.image,
-    // data.additionalData
-    alert(data.message);
-});
-
-push.on('error', function(e) {
-    // e.message
-    alert(e.message);
-});
-        
+        var pushNotification = window.plugins.pushNotification;
+		pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"486403283342","ecb":"app.onNotificationGCM"});   
+    },
+    // result contains any message sent from the plugin call
+	successHandler: function(result) {
+    	alert('Callback Success! Result = '+result)
+	},
+	errorHandler:function(error) {
+    	alert(error);
+	},
+	onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
     }
 };
